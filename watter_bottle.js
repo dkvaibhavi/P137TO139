d02557cb6aa8ca7fe23f8a91821b7257852d68ba
dkvaibhavi <98399336+dkvaibhavi@users.noreply.github.com>
2023-07-11 15:58:35 +0530

    Add files via upload

---
+++ watter_bottle.js
@@ -0,0 +1,45 @@
+function setup() {
+    canvas = createCanvas(640,420);
+   canvas.center();
+   objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+img = "";
+modelStatus = "";
+objects = [];
+
+function preload() {
+img = loadImage("https://i.postimg.cc/pd5r0TNR/pastel-blue-light-pale-powder-blue-solid-color-water-bottles.webp")
+}
+
+function draw() {
+   image(img , 0 , 0 , 640 , 420);
+
+   if(modelStatus != ""){
+       for(i = 0; i < objects.length; i++){
+           document.getElementById("status").innerHTML = "Status : Object Detected";
+           fill("#bd73fa");
+           percent = floor(objects[i].confidence * 100);
+           text(objects[i].label + " " + percent + "%" , objects[i].x, objects[i].y);
+           noFill();
+           stroke("#bd73fa")   
+           rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);    
+       }
+   }
+}
+
+function modelLoaded() {
+   console.log("Model Loaded!!!")
+   modelStatus = true;
+   objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error , results) {
+   if (error) {
+       console.log(error);
+   }
+   console.log(results);
+   objects = results;
+}
+
